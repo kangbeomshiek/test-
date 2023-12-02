1e6c3f2499a2d9bd0e8e67fde8b3f7af891dfd17
kangbeomshiek <152569367+kangbeomshiek@users.noreply.github.com>
2023-12-02 19:46:31 +0900

    Qiz
    Qiz 1

--- Main.java
+++ Main.java
@@ -1,10 +1,24 @@
-package java;
+package com.java;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		   for ( long i = 0,  n = 1; i<= 10; i++ , n *=10) {				//10의 반복과 제곱의 반복을 위한 문
+			   
+			   						
+			   System.out.println("10^" +String.format("%2d", i)  + "=" + String.format("%12d",n) );  //출력문
+			   
+			   
+			   
+			   
+			   
+		   }
+		   
+		
+		
+		
 	}
 
 }
